d03f6db375a361ea39a922c45ff8ce35a613368b
Adan Nugraha <contact.ardana@gmail.com>
2022-09-12 19:11:04 +0700

    validators and error exceptions added 2 validators for songs and albums request, added 3 error exception

--- src/server.js
+++ src/server.js
@@ -1,1 +1,41 @@
 require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const albums = require('./api/albums');
+const SongsService = require('./services/postgres/SongsService');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const SongsValidator = require('./validator/songs');
+const AlbumsValidator = require('./validator/albums');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    await server.register({
+        plugin: songs,
+        options: {
+            service: SongsService,
+            validator: SongsValidator,
+        },
+    });
+
+    await server.register({
+        plugin: albums,
+        options: {
+            service: AlbumsService,
+            validator: AlbumsValidator,
+        },
+    });
+
+    await server.start();
+    console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
